043f7e69f56d3970ff8c60122c9c441cac6f0d1e
Mauricio Vargas <mavargas11@uc.cl>
2023-04-03 20:59:49 -0400

    don't hide title

--- functions.js
+++ functions.js
@@ -1,6 +1,9 @@
 // hide all divs except the one with the id passed in
 function showDiv(id) {
     var divs = document.getElementsByTagName("div");
+    divs = Array.from(divs).filter(function (div) {
+        return div.id != "title";
+    });
     for (var i = 0; i < divs.length; i++) {
         if (divs[i].id == id) {
             divs[i].style.display = "block";
